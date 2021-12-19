6a80e6c32245f012a1e5b38f9eea99cafd20ce4f
Ilya <zaytsevilia.a@gmail.com>
2021-12-19 20:23:24 +0300

    lesson3

--- geekshop/views.py
+++ geekshop/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
+from mainapp.models import Product
 
 
 def index(request):
     title = 'Магазин'
+    products = Product.objects.all()[:4]
+
     context = {
         'title': title,
+        'products': products,
     }
-    return render(request, 'geekshop/index.html')
+    return render(request, 'geekshop/index.html', context)
 
 def contacts(request):
     title = 'Контакты'
     context = {
         'title': title,
     }
-    return render(request, 'geekshop/contacts.html')
+    return render(request, 'geekshop/contacts.html', context)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+from mainapp.models import Product
+
+
 def products(request):
     title = 'Каталог'
+    products = Product.objects.all()[:5]
+
     links_menu = [
         {'href': 'products_all', 'name': 'Всё'},
         {'href': 'products_home', 'name': 'Дом'},
@@ -12,5 +17,6 @@ def products(request):
     context = {
         'title': title,
         'links_menu': links_menu,
+        'products': products,
     }
     return render(request, 'mainapp/products.html', context=context)
